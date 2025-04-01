e7fe254e44ecce7a08a2be0492269cb0c4e09670
EduMelo <dev@edumelo.com.br>
2025-03-31 22:20:41 -0300

    fixed default value of autorun

--- src/background/chrome/background.ts
+++ src/background/chrome/background.ts
@@ -1,5 +1,14 @@
 /// <reference types="chrome"/>
 
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.get("reopenProcessCenterEnabled", (data) => {
+    if (data.reopenProcessCenterEnabled === undefined) {
+      chrome.storage.sync.set({ reopenProcessCenterEnabled: true });
+    }
+  });
+});
+
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'OPEN_PROCESSCENTER') {
     if (sender.tab && typeof sender.tab.index === 'number') {
